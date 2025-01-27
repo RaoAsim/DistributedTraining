54a4dffa4decd8051c00ef8277d1c895e66e3c93
RaoAsim <80324350+RaoAsim@users.noreply.github.com>
2025-01-27 13:39:13 +0500

    Update config.py

--- distributed_training/utils/config.py
+++ distributed_training/utils/config.py
@@ -227,7 +227,7 @@ def add_args(cls, parser, prefix=None):
             "--neuron.training_examples_per_miner",
             type=int,
             help="The number of rows to train on per miner",
-            default=900,
+            default=400,
         )
 
         parser.add_argument(
