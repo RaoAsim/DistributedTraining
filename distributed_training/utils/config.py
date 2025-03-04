682eb47008d6b463a2a96b0c1285030bde37f14c
RaoAsim <80324350+RaoAsim@users.noreply.github.com>
2025-03-04 13:05:48 +0500

    Update config.py

--- distributed_training/utils/config.py
+++ distributed_training/utils/config.py
@@ -196,7 +196,7 @@ def add_args(cls, parser, prefix=None):
         "--neuron.local_batch_size_train_effective",
         type=int,
         help="Amount of micro batches for gradient accumulation",
-        default=1024,
+        default=1040,
     )
 
     parser.add_argument(
